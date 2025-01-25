cc2eaf74e923c4f65b72aab1d46ac288942c3be3
telcrolove <telcrolove@yahoo.co.jp>
2025-01-25 18:48:00 +0900

    kadai_020 提出

---
+++ kadai_020/event.js
@@ -0,0 +1,11 @@
+const addBtn = document.getElementById('btn')
+
+addBtn.addEventListener('click', () =>{
+
+    const changeWord = document.getElementById('text');
+
+    //changeWord.textContent =  'ボタンをクリックしました';
+
+    changeWord.textContent ='ボタンをクリックしました';
+
+});
